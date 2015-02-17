4df5998f3556df774e44afd8cb524773106a66e1
Grace Purcell <gracecpurcell@hotmail.com>
2015-02-17 16:57:33 +0000

    Initial Commit

--- src/com/example/app/model/DBConnection.java
+++ src/com/example/app/model/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static Connection sConnection;
-
+    // Implement the DBConnection class as a singleton.
     public static Connection getInstance() throws ClassNotFoundException, SQLException {
         String host, db, user, password;
         
